c9e824449f025c4eb8696176006bc931002caebd
chaitanyapunwatkar <chaitanya.punwatkar@gmail.com>
2022-12-26 19:03:29 +0530

    Comments and DocStrings are added

--- main_gui.py
+++ main_gui.py
@@ -7,6 +7,12 @@ from math import ceil
 from GraphView import PieChart
 
 class ImagePal(QWidget):
+    """ This class is about representing the product images with color palette below it. 
+    This is GridLayout, Vertical layout is added to show color palette below image.
+    Args:
+        QWidget (_type_): The QWidget is the superclass inherited to use its methods 
+        like QVBoxLayout, QHBoxLayout, QLabel.
+    """
     def __init__(self, c, color):
         super(ImagePal, self).__init__()
         self.setAutoFillBackground(True)
@@ -15,17 +21,22 @@ class ImagePal(QWidget):
         self.label = QLabel(self)
         self.img2 = self.img.scaled(128,128)
         self.label.setPixmap(self.img2)
-        
         self.ind_label = QLabel(" ", self)
         self.ind_label.setStyleSheet("background-color: {0};width: 6px; margin: 0px px 0px 0px;".format(color))
         self.Vlayout.addWidget(self.label)
-        # self.label.setAlignment(Qt.AlignCenter)
         self.Vlayout.addWidget(self.ind_label)
-        # self.ind_label.setAlignment(Qt.AlignCenter)
+        # Size of the color palette is fixed
         self.ind_label.setFixedSize(130,10)
        
 
 class Gallery_All(QWidget):
+    """ This Class is called when 'All' Status is selected in the Image Gallery tab. Here All the Images 
+        present in the database for particular SKU ID are showcased.
+
+    Args:
+        QWidget (Class Imprted from PySide2.QtWidget): The QWidget is the superclass inherited to use its methods like QVBoxLayout, 
+        QStackedWidget, QScrollArea.
+    """
     def __init__(self):
             super(Gallery_All, self).__init__()
             self.setAutoFillBackground(True)
@@ -59,6 +70,13 @@ class Gallery_All(QWidget):
 
 
 class Gallery_Bad(QWidget):
+    """ This Class is called when 'Bad' Status is selected in the Image Gallery tab. This will showcase product Images
+        whose quality is labeled as Bad.
+
+    Args:
+        QWidget (Class Imprted from PySide2.QtWidget): The QWidget is the superclass inherited to use its methods like 
+        QVBoxLayout, QStackedWidget, QScrollArea.
+    """
     def __init__(self):
             super(Gallery_Bad, self).__init__()
             self.setAutoFillBackground(True)
@@ -71,8 +89,6 @@ class Gallery_Bad(QWidget):
             self.scrollArea.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
             self.scrollArea.setWidgetResizable(True)
             self.Glayout = QGridLayout(self.scrollWidgetContents)
-            self.Glayout.setSpacing(0)
-            self.Glayout.setContentsMargins(0,0,0,0)
             self.scrollArea.setWidget(self.scrollWidgetContents)
             self.Vlayout.addWidget(self.scrollArea)
             text_select ='Bad'
@@ -91,6 +107,13 @@ class Gallery_Bad(QWidget):
             self.setLayout(self.Vlayout)
 
 class Gallery_Good(QWidget):
+    """ This Class is called when 'Good' Status is selected in the Image Gallery tab. This will showcase product Images
+        whose quality is labeled as Good.
+
+    Args:
+        QWidget (Class Imprted from PySide2.QtWidget): The QWidget is the superclass inherited to use its methods like QVBoxLayout, 
+        QStackedWidget, QScrollArea.
+    """
     def __init__(self):
             super(Gallery_Good, self).__init__()
             self.setAutoFillBackground(True)
@@ -120,36 +143,58 @@ class Gallery_Good(QWidget):
             self.setLayout(self.Vlayout)
  
  
-# Analysis Page UI components
 class Analysis(QWidget):
+    """ This Class represents the Analytics Page Tab and includes necessary UI components for the same.
+        Horizontal Layout is added inside Vertical Layout includes SKU ID label and
+        DropDown list. Below horizontal Layout GraphFrame widget is added.
+        
+    Args:
+        QWidget (Class Imported from Pyside2.QtWidget): This class is inherited to use methods 
+        like StackedLayout(), ScrollArea(), QVBoxLayout(), QComboBox(), QLabel().
+        
+    """
     def __init__(self):
         super(Analysis, self).__init__()
         self.setAutoFillBackground(True)
         self.Vlayout = QVBoxLayout(self) 
         self.Hlayout = QHBoxLayout(self)
-        #self.Hlayout.setStretch()
+
         self.Vlayout.addLayout(self.Hlayout)
         label_t = QLabel("SKU ID: ", self)
         dropBox = QComboBox(self)
-        # label_t.setGeometry(10,10,100, 50)
-        # dropBox.setGeometry(65,10,200,50)
         dropBox.addItems(['Item1', 'Item2'])
         self.Hlayout.addWidget(label_t)
         self.Hlayout.addWidget(dropBox)
-        self.Hlayout.setStretch(1,0)
+        self.Hlayout.setAlignment(Qt.AlignRight)
         dropBox.currentTextChanged.connect(self.print_drop_text)
         self.Vlayout.addWidget(PieChart())
+        self.Vlayout.setAlignment(Qt.AlignTop)
         self.setLayout(self.Vlayout)
         
          
     def print_drop_text(self, text_select):
+        """ This function will set the chart according to SKU ID selected. 
+
+        Args:
+            text_select (String): A variable which contains SKU ID text selected bu user.
+        """
         print(text_select)
         
         
-#Gallery Page UI components
-class Gallery(QWidget):
+
+class Gallery(QWidget): 
+    """This class is called when user selects the Image Gallery Tab. This includes necessary 
+        UI Components for Gallery Page. This includes VerticalLayout where top most widget includes filter option
+        and another layout includes stacked widget with scroll area where images placed according to filter option
+        selected by user
+
+    Args:
+        QWidget (Class Imported from Pyside2.QtWidget): This class is inherited to use methods 
+        like StackedLayout(), ScrollArea(), QVBoxLayout(), QComboBox(), QLabel().
+    """
     def __init__(self):
         super(Gallery, self).__init__()
+        
         self.setAutoFillBackground(True)
         self.Vlayout = QVBoxLayout(self) 
         self.Hlayout = QHBoxLayout(self)
@@ -166,9 +211,14 @@ class Gallery(QWidget):
         self.Vlayout.addLayout(self.Hlayout)
         self.Hlayout.addWidget(status_filter)
         self.Vlayout.addWidget(self.Stack)
-        status_filter.currentTextChanged.connect(self.print_drop_text)
+        status_filter.currentTextChanged.connect(self.set_index)
     
-    def print_drop_text(self, text_select):
+    def set_index(self, text_select):
+        """This function is used to set particular Index of StackWidget according to user selected status filter.
+
+        Args:
+            text_select (String): This variable contains Text from User selects from DropDown box.
+        """
         if text_select == 'Good':
             self.Stack.setCurrentIndex(1)
         elif text_select == 'Bad':
@@ -176,32 +226,38 @@ class Gallery(QWidget):
         else:
             self.Stack.setCurrentIndex(0)                    
 
-#Window Page - Parent Class        
+       
 class MainWindow(QMainWindow):
+    """ This is the Parent Window Class, To set the MainWindows settings and UI components.
+    
+    Args:
+        QMainWindow (Class inherits from QWidget): To use methods like setWindowTitle(), QTabWidget()
+    """
     def __init__(self):
         super().__init__()
+        """ We have created two tabs 
+            1. Analytics Tab - Chart View of the Product Inspection
+            2. Image Gallery Tab - View images of Inspected with color label 
+               indicating the status of product good or bad.
+        """
         
         self.setWindowTitle("Post_Inspection Analysis")
         tabs = QTabWidget()
         tabs.setTabPosition(QTabWidget.North)
         tabs.setMovable(True)
-        tabs.addTab(Analysis(), "Analysis Tab")
+        tabs.addTab(Analysis(), "Analytics Tab")
         tabs.addTab(Gallery(), "Image Gallery Tab")
-
         self.setCentralWidget(tabs)
-
-    # def activate_tab_1(self):
-    #     self.stacklayout.setCurrentIndex(0)
-
-    # def activate_tab_2(self):
-    #     self.stacklayout.setCurrentIndex(1)
 
 
 if __name__ == "__main__":
+    """ This part of code is excuted first when we run the code.
+    """
+    
     app = QApplication(sys.argv)
 
     window = MainWindow()
     window.setFixedSize(1100,800)
     window.show()
-
+    #Below line opens the App
     app.exec_()
